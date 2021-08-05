c459b1fed021be19f0f101613b67c234163d0cc4
CharlieBerens <38572813+Yeah-No@users.noreply.github.com>
2021-08-04 20:43:02 -0600

    Changed from / to window.location.href

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -48,7 +48,7 @@ export default class Form extends Component {
 
     submitForm = () => {
         const axiosOptions = {
-            url: '/',
+            url: window.location.href,
             method: "post",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             data: qs.stringify({
